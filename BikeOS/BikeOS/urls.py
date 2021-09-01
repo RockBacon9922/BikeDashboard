b2020f2caa8523b5f88c609c1b3c4a31e9b80060
RockBacon9922 <58570245+RockBacon9922@users.noreply.github.com>
2021-09-01 15:19:37 +0100

    Initial Commit

--- BikeOS/BikeOS/urls.py
+++ BikeOS/BikeOS/urls.py
@@ -1,4 +1,4 @@
-"""backend URL Configuration
+"""BikeOS URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -14,14 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from rest_framework import routers
-from todo import views
-
-router = routers.DefaultRouter()
-router.register(r'todos', views.TodoView, 'todo')
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
 ]
