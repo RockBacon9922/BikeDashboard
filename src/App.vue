6fa68be2f2c06485debd77988ba52244b7dbe8e1
RockBacon9922 <58570245+RockBacon9922@users.noreply.github.com>
2021-10-17 18:25:31 +0100

    Now using vite again and made a template app using tailwind!!!!

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import Dashboard from './components/Dashboard.vue';
+import './index.css'
+</script>
+
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view />
+  <Dashboard />
 </template>
 
 <style>
@@ -11,20 +14,5 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
---
+++ src/components/Dashboard.vue
@@ -0,0 +1,32 @@
+<style>
+.bgColor {
+  @apply bg-gray-600;
+}
+.navColors {
+  @apply text-black bg-green-600 opacity-30 rounded-3xl gap-10;
+}
+
+</style>
+
+
+<script>
+import { ref } from "vue";
+
+const time = ref(2000)
+
+</script>
+
+
+<template>
+  <div id="main" class="bgColor min-h-screen min-w-min flex flex-col">
+    <nav class="relative p-2 m-2 flex justify-between flex-wrap flex-row navColors">
+      <h1 class="justify-self-start">User</h1>
+      <h1 class="justify-self-center">time and date</h1>
+      <h1 class="justify-self-end">100%</h1>
+    </nav>
+    <div id="main" class="relative flex-auto rounded-3xl m-2 bg-blue-600"></div>
+  </div>  
+</template>
+
+<style scoped>
+</style>
